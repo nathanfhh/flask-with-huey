c68a34932e215a574309a1d08619f99a47850096
傅暄桓 <t13762@ms.sltung.com.tw>
2021-11-19 13:29:17 +0800

    update: add in pre-hook and post-hook for huey. update: add more signals to signal handler.

--- huey_task/task.py
+++ huey_task/task.py
@@ -8,15 +8,27 @@ huey_db_path = Path("huey_demo.db").absolute()
 huey = SqliteHuey(name="Huey Demo", fsync=True, filename=huey_db_path, utc=False)
 
 
+@huey.pre_execute()
+def pre_step(task):
+    print("PRE-Step", task.name)
+
+
 @huey.task(retries=1, retry_delay=5, context=True)
 def add_two_numbers_that_need_a_long_period_of_time(number_a, number_b, task=None):
     time.sleep(8)
     return int(number_a) + int(number_b)
 
 
-@huey.signal(signals.SIGNAL_ERROR)
-def task_error(signal, task, exception):
-    message = f"Task:[{task.name}] failed!\nTaskID: [{task.id}]\n" \
-              f"Args:[{task.args}]\nKwargs: [{task.kwargs}]\n" \
-              f"Exception: [{exception}][{traceback.format_exc()}]\nRetries: [{task.retries}]"
-    logging.critical(message)
+@huey.post_execute()
+def post_hook(task, task_value, exc):
+    print("POST-Step", task, task_value, exc)
+
+
+@huey.signal(signals.SIGNAL_ERROR, signals.SIGNAL_SCHEDULED, signals.SIGNAL_EXECUTING, signals.SIGNAL_COMPLETE)
+def signal_handler(signal, task, exception=None):
+    if signal == "error":
+        message = f"Task:[{task.name}] failed!\nTaskID: [{task.id}]\n" \
+                  f"Args:[{task.args}]\nKwargs: [{task.kwargs}]\n" \
+                  f"Exception: [{exception}][{traceback.format_exc()}]\nRetries: [{task.retries}]"
+        logging.critical(message)
+    print("SIGNAL", signal, task, exception)
